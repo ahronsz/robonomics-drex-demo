cc99997c0325f7b1ff24249474d86c394533005f
Ahron Genaro Sotomayor Zegarra <ahron.sotomayor@globant.com>
2022-09-09 01:19:04 -0500

    add rest back

---
+++ services/rest.py
@@ -0,0 +1,23 @@
+import logging
+import requests
+import json
+import logging
+
+base_url = "http://drex-env.eba-jkxuyqbq.us-east-1.elasticbeanstalk.com/grid"
+record_url = base_url + "/record"
+
+
+def record_log(body_request):
+    _headers = {'Content-Type': 'application/json'}
+    try:
+        r = requests.post(record_url, data=json.dumps(body_request), headers=_headers)
+        logging.info("Operation Successful. status code: ", r.status_code())
+    except requests.exceptions.RequestException as err:
+        print('Bad Status Code', r.status_code)
+        logging.warning(err)
+
+
+
+
+
+
