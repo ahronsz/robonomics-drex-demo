a8ba192ed86596910d7bcf2ad4f6f376826c2cb3
Ahron Genaro Sotomayor Zegarra <ahron.sotomayor@globant.com>
2022-09-14 12:27:43 -0500

    last change

--- services/rest.py
+++ services/rest.py
@@ -10,12 +10,14 @@ record_url = base_url + "/record"
 def record_log(body_request):
     _headers = {'Content-Type': 'application/json'}
     try:
+        print("entra a rest")
+        print(body_request)
         r = requests.post(record_url, data=json.dumps(body_request), headers=_headers)
         logging.info("Operation Successful. status code: ", r.status_code())
     except requests.exceptions.RequestException as err:
         print('Bad Status Code', r.status_code)
         logging.warning(err)
-
+<
 
 
 
