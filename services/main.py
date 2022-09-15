bf40790f1031f7fd5622af268686c35d03825026
Ahron Genaro Sotomayor Zegarra <ahron.sotomayor@globant.com>
2022-09-14 23:44:18 -0500

    last change

--- services/main.py
+++ services/main.py
@@ -4,9 +4,7 @@ import sys
 import rest
 import rpi_funcs as rpi
 
-from robonomicsinterface import Account
-from robonomicsinterface import Datalog
-#from substrateinterface import Keypair
+from robonomicsinterface import Account, Datalog
 
 # set up logging
 logging.basicConfig(
@@ -16,32 +14,20 @@ logging.basicConfig(
 
 # Define Robonomics sss58_address from seed
 seed: str = sys.argv[1]
-#keypair = Keypair.create_from_mnemonic(seed, ss58_format=2)
-account_with_seed = Account(seed=seed, remote_ws="")
-
-# Start income tracker
-#income_tracker = DrxIncomeTracker(keypair.ss58_address)
+account_with_seed = Account(seed=seed, remote_ws="ws://127.0.0.1:9944")
 
 # Start solar panel daemon
 logging.info("Started main Solar panel daemon")
 while True:
-    # wait for money income event
-    #income_tracker.act_income_event.wait()
-    #income_tracker.act_income_event.clear()
-    
-
     #if operation["success"]:
     logging.info("Operation Successful.")
     try:
         operation = rpi.get_log()
-        # Initiate RobonomicsInterface instance
-        #ri_interface = RI(seed=seed, remote_ws="wss://kusama.rpc.robonomics.network")
         rest.record_log(operation)
-        #ri_interface = RI(seed=seed, remote_ws="ws://127.0.0.1:9944")
         datalog = Datalog(account_with_seed)
-        datalog.record("Hello, world")
-          # If index was not provided here, the latest one will be used
-        logging.info(f"Successfully! {datalog.get_item(account_with_seed.get_address())}")
+        datalog.record(f"Successfully! {operation}")
+        lastDatalog = datalog.get_item(account_with_seed.get_address())  # If index was not provided here, the latest one will be used
+        logging.info(f"Successfully! {lastDatalog.get}")
         #ri_interface.record_datalog(f"Successfully! {operation}")
         #if operation["success"]:
         #    print("hola mundo")
